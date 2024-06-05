cde7dc3acbe35bfcf963810c4a44c73d4bfeda32
Riddhi Patel <riddhi@Riddhis-MacBook-Air.local>
2024-06-05 16:11:24 +0530

    sample commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function addNewWEField() {
+//    console.log("Adding");
+
+let newNode=document.createElement('textarea');
+newNode.classList.add('form-control');
+newNode.classList.add('weField');
+newNode.setAttribute("rows",3);
+
+let weOb=document.getElementById("we");
+let weAddButtonOb=document.getElementsById("weAddButton");
+
+}
